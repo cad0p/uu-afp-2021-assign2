95a8db9b70c47e47aa0e346a7cf5fb2f185223b5
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-02-25 05:13:02 +0100

    RoseTree Functor

--- assign2-cad0p/src/Assign2/RoseTree.hs
+++ assign2-cad0p/src/Assign2/RoseTree.hs
@@ -33,3 +33,18 @@ data RoseTree a = RoseNode a [RoseTree a] | RoseLeaf
   deriving Show
 
 
+{-|
+  https://stackoverflow.com/questions/69695834/defining-fmap-for-a-rosetree
+
+  >>> *Assign2.RoseTree> fmap (+1) (RoseNode 1 [(RoseNode 3 [])])
+  >>  RoseNode 2 [RoseNode 4 []]
+-}
+instance Functor RoseTree where
+  fmap _ RoseLeaf = RoseLeaf
+  fmap f (RoseNode a rs) =
+    RoseNode (f a) ((fmap . fmap) f rs)
+
+instance Functor [] where
+  fmap _ []       = []
+  fmap f (x : xs) = f x : fmap f xs
+
