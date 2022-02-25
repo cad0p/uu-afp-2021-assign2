3b9efd5b878cd7b3c71a65f82cf775a2913cada4
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-02-25 06:19:41 +0100

    stack test fails test case

--- assign2-cad0p/test/Spec.hs
+++ assign2-cad0p/test/Spec.hs
@@ -1,6 +1,8 @@
 import          Test.Tasty
 import          Test.Tasty.QuickCheck         as QC
-import          Test.Tasty.QuickCheck.Laws.Functor (testFunctorLaws)
+import          Test.Tasty.HUnit              as HU
+import Assign2.RoseTree (RoseTree(..))
+-- import          Test.Tasty.QuickCheck.Laws.Functor (testFunctorLaws)
 
 -- import qualified Assign2 as A2
 
@@ -8,7 +10,7 @@ main :: IO ()
 main = defaultMain tests
 
 tests       ::  TestTree
-tests       =   testGroup "Tests"       [ properties ]
+tests       =   testGroup "Tests"       [ properties, unitTests ]
 
 properties  ::  TestTree
 properties  =   testGroup "Properties"  [ qcProps ]
@@ -29,3 +31,33 @@ qcRoseTree  =   testGroup "RoseTree" []
 
 qcTeletype  ::  TestTree
 qcTeletype  =   testGroup "Teletype" []
+
+
+
+unitTests   ::  TestTree
+unitTests   =   testGroup "Unit tests"  [ hUnit ]
+
+hUnit       ::  TestTree
+hUnit       =   testGroup "HUnit"   [ huTree
+                                    , huRoseTree
+                                    , huTeletype ]
+
+huTree      ::  TestTree
+huTree      =   testGroup "Tree"
+  [ -- hu.testProperty "Functor Laws" []
+      -- (testFunctorLaws A2.Functor A2.Tree)
+  ]
+
+huRoseTree  ::  TestTree
+huRoseTree  =   testGroup "RoseTree" [ huRoseTreeApplicative ]
+
+huRoseTreeApplicative
+            :: TestTree
+huRoseTreeApplicative
+            =   testGroup "Applicative"
+              [ testCase  "RoseLeaf r"
+                  (RoseNode (+1) [RoseNode (*2) []] <*> RoseLeaf @?= RoseLeaf)
+              ]
+
+huTeletype  ::  TestTree
+huTeletype  =   testGroup "Teletype" []
