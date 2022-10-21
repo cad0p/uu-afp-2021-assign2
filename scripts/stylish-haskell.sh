ad6a4b573a713a962330e358fc8041c6c17493bc
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-10-21 14:01:28 +0200

    echo both releases and url url was only github.com..

--- scripts/stylish-haskell.sh
+++ scripts/stylish-haskell.sh
@@ -7,7 +7,11 @@ PACKAGE=stylish-haskell
 echo Downloading and running $PACKAGE...
 
 RELEASES=$(curl --silent https://github.com/haskell/$PACKAGE/releases)
+echo "releases:"
+echo $RELEASES
 URL=https://github.com/$(echo $RELEASES | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
+echo "url:"
+echo $URL
 VERSION=$(echo $URL | sed -e 's/.*-\(v[\.0-9]\+-linux-x86_64\)\.tar\.gz/\1/')
 TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
 
@@ -17,7 +21,6 @@ cleanup(){
 trap cleanup EXIT
 
 curl --progress-bar --location -o$TEMP/$PACKAGE.tar.gz $URL
-echo $URL
 file $TEMP/$PACKAGE.tar.gz
 tar -xzf $TEMP/$PACKAGE.tar.gz -C$TEMP
 $TEMP/$PACKAGE-$VERSION/$PACKAGE $*
