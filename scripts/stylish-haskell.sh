2c79dc5ef824064b7868ed2a94dbce5de6b937d3
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-10-21 16:15:48 +0200

    get latest release

--- scripts/stylish-haskell.sh
+++ scripts/stylish-haskell.sh
@@ -6,10 +6,8 @@ set -e
 PACKAGE=stylish-haskell
 echo Downloading and running $PACKAGE...
 
-RELEASES=$(curl --silent https://github.com/haskell/$PACKAGE/releases)
-echo releases:
-echo $RELEASES
-URL=https://github.com/$(echo $RELEASES | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
+RELEASE=$(curl --silent https://github.com/haskell/$PACKAGE/releases | egrep '[^\"]*/v[0-9|.]+' -m 1 -o)
+URL=https://github.com/$(echo $RELEASE | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
 echo url:
 echo $URL
 VERSION=$(echo $URL | sed -e 's/.*-\(v[\.0-9]\+-linux-x86_64\)\.tar\.gz/\1/')
